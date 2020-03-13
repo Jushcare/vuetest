b81efe3b26c949df44927cb836e3c2a015e8e772
Timmy <jushcare@gmail.com>
2020-03-13 11:34:57 +0100

    Update Assessment.vue

--- src/components/Assessment.vue
+++ src/components/Assessment.vue
@@ -1,60 +1,94 @@
 <template>
 
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-md-6 col-sm-6 col-xs-12" id="userTable" style="display:non">
-      <div class="caption"> Application Pending Assessment</div>
-    <div class="table-responsive">
-      <table class="table table-striped table-bordered">
-        <thead class="indigo text-white">
+<div id="content" class="p-4">
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="">Dashboard</div>
+  </div>
+    <div class="row">
+      <div class="col-md-6 col-xs-12" id="userTable">
+        <div class="caption"> Application Pending Assessment</div>
+      <div class="table-responsive">
+        <table class="table table-striped table-bordered">
+          <thead class="indigo text-white">
+            <tr>
+              <th>SN</th>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
           <tr>
-            <th>SN</th>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>Action</th>
+            <td>1</td>
+            <td>Johnson Johnson</td>
+            <td>0123456789</td>
+      <td><a href="#" class="btn btn-info"> Process </a></td>
           </tr>
-        </thead>
-        <tbody>
-          <tr v-for="data in userDatas" :key="data.id">
-            <td>{{data.id}}</td>
-            <td>{{data.name}}</td>
-            <td>{{data.phone}}</td>
-            <td><button class="btn btn-info btn-xs" v-on:click = "processData">Process</button></td>
+          <tr>
+            <td>1</td>
+            <td>Johnson Johnson</td>
+            <td>0123456789</td>
+      <td><a href="#" class="btn btn-info"> Process </a></td>
           </tr>
-        </tbody>
-      </table>
-    </div>
-    </div>
+          </tbody>
+        </table>
+      </div>
+      </div>
 
 
-    <div class="col-md-6 col-sm-6 col-xs-12" v-if="processData">
+<div class="col-md-6 col-xs-12">
     <div class="table-caption"> Application Processed</div>
-    <div class="table-responsive">
-    <table class="table table-striped table-bordered">
-    <thead class="indigo text-white">
-      <tr>
-        <th>SN</th>
-        <th>Name</th>
-        <th>Phone</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>Johnson Johnson</td>
-        <td>0123456789</td>
-        <td><a href="#" class="btn btn-info"> Processed </a> <a href="#" class="btn btn-primary"> View </a></td>
-      </tr>
-      <tr>
-    </tbody>
-    </table>
-    </div>
-    </div>
+      <div class="table-responsive">
+      <table class="table table-striped table-bordered">
+      <thead class="indigo text-white">
+        <tr>
+          <th>SN</th>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Johnson Johnson</td>
+          <td>0123456789</td>
+    <td><a href="#" class="btn btn-info"> Processed </a> <a href="#" class="btn btn-primary"> View </a></td>
+        </tr>
+      </tbody>
+      </table>
+      </div>
+   </div>
 
+</div>
 </div>
 
 
 </div>
 
 </template>
+
+<script>
+//import VueRouter from 'vue-router';
+//const route = new VueRouter;
+//export default {
+//  name: 'assessment',
+//  data() {
+  //  return {
+  //    userDatas: [
+//        { id: 1, name: "Johnson Johnson", phone: '0123456789' },
+  //      { id: 2, name: "Johnson Johnson2", phone: '0123456789' },
+  //      { id: 3, name: "Johnson Johnson3", phone: '0123456789'},
+  //    ]
+//  },
+
+//  methods : {
+//    processData() {
+//      route.push('process');
+//    }
+//  },
+
+//}
+</script>
